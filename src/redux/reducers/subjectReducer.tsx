9d09fd4eaa7f0ddbef8c8a1890e3d9ce8ed40b01
khaitran <akashikazuo99@gmail.com>
2022-10-05 21:44:57 +0700

    tao folder Test + subjectReducer

---
+++ src/redux/reducers/subjectReducer.tsx
@@ -0,0 +1,15 @@
+import { createSlice } from '@reduxjs/toolkit'
+
+const initialState = {
+
+}
+
+const subjectReducer = createSlice({
+  name: "subjectReducer",
+  initialState,
+  reducers: {}
+});
+
+export const {} = subjectReducer.actions
+
+export default subjectReducer.reducer
