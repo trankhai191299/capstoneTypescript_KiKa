6c4c080e15f8e640da543683e0dc8c2eac25bb9c
kaylee182000 <trungkien40414@gmail.com>
2022-10-07 23:42:03 +0700

    login done

--- src/pages/Login/Login.tsx
+++ src/pages/Login/Login.tsx
@@ -1,9 +1,85 @@
-import React from 'react'
-
-type Props = {}
+import { useFormik } from "formik";
+import React from "react";
+import { useDispatch } from "react-redux";
+import { AppDispatch, RootState } from "../../redux/configStore";
+import { loginApi, UserModel } from "../../redux/reducers/userReducer";
+import * as Yup from "yup";
+import { NavLink } from "react-router-dom";
+type Props = {};
 
 export default function Login({}: Props) {
+  const dispatch: AppDispatch = useDispatch();
+  const frm = useFormik({
+    initialValues: {
+      account: "",
+      password: "",
+    },
+    validationSchema: Yup.object().shape({
+      account: Yup.string().required("Tai khoan không được bỏ trống"),
+      password: Yup.string()
+        .required("Mật khẩu không được bỏ trống")
+        .min(6, "Password có độ dài từ 6 đến 32 ký tự")
+        .max(32, "Password có độ dài từ 6 đến 32 ký tự"),
+    }),
+    onSubmit: (values: UserModel) => {
+      dispatch(loginApi(values));
+    },
+  });
   return (
-    <div>Login</div>
-  )
+    <div className="container">
+      <div className="form-signin w-100 m-auto">
+        <form className="mt-5 mb-5" onSubmit={frm.handleSubmit}>
+          <h2 className=" mb-5 fw-semibold text-center">-Please Sign In-</h2>
+          <div className="form-floating">
+            <input
+              type="account"
+              className="form-control"
+              id="account"
+              onChange={frm.handleChange}
+              onBlur={frm.handleBlur}
+            />
+            <label htmlFor="floatingInput">Account</label>
+            {frm.errors.account ? (
+              <span className="text-danger fs-6 fw-lighter fst-italic ms-1 text-capitalize">
+                {frm.errors.account}
+              </span>
+            ) : (
+              ""
+            )}
+          </div>
+          <div className="form-floating">
+            <input
+              type="password"
+              className="form-control"
+              id="password"
+              onChange={frm.handleChange}
+              onBlur={frm.handleBlur}
+            />
+            <label htmlFor="floatingPassword">Password</label>
+            {frm.errors.password ? (
+              <span className="text-danger fs-6 fw-lighter fst-italic ms-1 text-capitalize">
+                {frm.errors.password}
+              </span>
+            ) : (
+              ""
+            )}
+          </div>
+          <div className="checkbox mt-4 mb-4">
+            <label>
+              <input type="checkbox" defaultValue="remember-me" /> Remember me
+            </label>
+          </div>
+          <button className="w-100 btn btn-lg btn-dark mt-3 mb-3">
+            Sign in
+          </button>
+          <p className="mt-5 mb-5 text-muted text-center">
+            Not a Member?
+            <NavLink to="/register" className="text-muted ms-1">
+              Join Us
+            </NavLink>
+          </p>
+        </form>
+      </div>
+    </div>
+  );
 }
--- src/redux/reducers/userReducer.tsx
+++ src/redux/reducers/userReducer.tsx
@@ -1,15 +1,52 @@
-import { createSlice } from '@reduxjs/toolkit'
+import { createSlice,PayloadAction } from "@reduxjs/toolkit";
+import { AppDispatch,RootState } from "../configStore";
+import {
+  setCookie,
+  getStore,
+  getStoreJson,
+  ACCESS_TOKEN,
+  http,
+  setStore,
+  setStoreJson,
+  USER_LOGIN,
+} from "../../utils/setting";
+import { history } from "../../index";
 
-const initialState = {
-    userLogin:[]
+export interface UserModel{
+  account: string,
+  password: string
 }
+const initialState:any = {
+  userLogin: getStoreJson(USER_LOGIN),
+};
 
 const userReducer = createSlice({
   name: "userReducer",
   initialState,
-  reducers: {}
+  reducers: {
+    getProfileAction: (state,action:PayloadAction<UserModel[]>) => {
+      state.userLogin = action.payload
+    }
+  },
 });
 
-export const {} = userReducer.actions
+export const {getProfileAction} = userReducer.actions;
 
-export default userReducer.reducer
+export default userReducer.reducer;
+
+export const loginApi = (userLogin:UserModel) => {
+  return async (dispatch:AppDispatch) => {
+    try {
+      const result = await http.post("/QuanLyNguoiDung/DangNhap", userLogin);
+
+      setCookie(ACCESS_TOKEN, result.data.accessToken, 30);
+      setStore(ACCESS_TOKEN, result.data.accessToken);
+      console.log(result.data)
+      history.push("/home");
+      //const action = getProfileApi();
+      //dispatch(action);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+};
