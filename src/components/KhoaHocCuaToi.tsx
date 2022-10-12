4bb41bb0e4746225e7ed1360f232580bbd45e2e0
khaitran <akashikazuo99@gmail.com>
2022-10-13 00:22:01 +0700

    UI khoa hoc cua toi

--- src/components/KhoaHocCuaToi.tsx
+++ src/components/KhoaHocCuaToi.tsx
@@ -4,6 +4,29 @@ type Props = {}
 
 export default function KhoaHocCuaToi({}: Props) {
   return (
-    <div>KhoaHocCuaToi</div>
+    <div className='my-course mb-4'>
+      <div className="container">
+        <h3 className='mt-4 main-title'>Các khóa học đã tham gia</h3>
+        
+        <div className="courses">
+          <div className="row">
+            <div className="col-12">
+              <div className="item">
+                <div className="row">
+                  <div className="col-2">
+                    <img src="https://picsum.photos/200/200" alt="..." />
+                  </div>
+                  <div className="col-10 position-relative">
+                    <h3 className='title'>Tên khóa học</h3>
+                    <p>Chi tiết khóa học ...</p>
+                    <button className='btn btn-danger position-absolute end-0'>Hủy khóa học</button>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
   )
 }
