41ba0501f40013ca37f3fdd15ba7c5a6d727a154
khaitran <akashikazuo99@gmail.com>
2022-10-28 00:07:06 +0700

    update UI course management

--- src/pages/Admin/CourseManagement/CourseManagement.tsx
+++ src/pages/Admin/CourseManagement/CourseManagement.tsx
@@ -1,72 +1,79 @@
 import React from 'react'
-import GhiDanhKhoaHoc from './GhiDanhKhoaHoc'
+import ConfirmList from './ConfirmList'
+import JoinList from './JoinList';
 type Props = {}
 
 export default function CourseManagement({}: Props) {
   return (
     <div className="container course-management">
-      {/* <div className="userContainer mt-5 d-none">
-        <button
-          type="button"
-          className="btn btn-dark btn-lg"
-          data-bs-toggle="modal"
-          data-bs-target="#modalId"
-        >
-          Thêm người dùng
-        </button>
+      <div
+        className="modal fade"
+        id="modalId"
+        tabIndex={-1}
+        data-bs-backdrop="static"
+        data-bs-keyboard="false"
+        role="dialog"
+      >
         <div
-          className="modal fade"
-          id="modalId"
-          tabIndex={-1}
-          data-bs-backdrop="static"
-          data-bs-keyboard="false"
-          role="dialog"
-          aria-labelledby="modalTitleId"
-          aria-hidden="true"
+          className="modal-dialog modal-lg modal-dialog-scrollable modal-dialog-centered"
+          role="document"
         >
-          <div
-            className="modal-dialog modal-dialog-scrollable modal-dialog-centered modal-xl"
-            role="document"
-          >
-            <div className="modal-content">
-              <div className="modal-header">
-                <h5 className="modal-title" id="modalTitleId">
-                  Them nguoi dung
-                </h5>
-                <button
-                  type="button"
-                  className="btn-close"
-                  data-bs-dismiss="modal"
-                  aria-label="Close"
-                />
+          <div className="modal-content">
+            <div className="modal-header">
+              <button
+                type="button"
+                className="btn-close"
+                data-bs-dismiss="modal"
+              />
+            </div>
+            <div className="modal-body">
+              <div className="content-1">
+                <h4>Chọn người dùng</h4>
+                <div className="form">
+                  <div className="form-group">
+                    <div className="row">
+                      <div className="col-10">
+                        <input
+                          className="form-control"
+                          type="text"
+                          placeholder="Tên người dùng"
+                        />
+                      </div>
+                      <div className="col-2">
+                        <button className="btn btn-light">Ghi danh</button>
+                      </div>
+                    </div>
+                  </div>
+                </div>
               </div>
-              <div className="modal-body">hi</div>
-              <div className="modal-footer">
-                <button
-                  type="button"
-                  className="btn btn-secondary"
-                  data-bs-dismiss="modal"
-                >
-                  Close
-                </button>
-                <button type="button" className="btn btn-primary">
-                  Save
-                </button>
+
+              <div className="content-2">
+                <ConfirmList />
+              </div>
+
+              <div className="content-3">
+                <JoinList />
               </div>
             </div>
           </div>
         </div>
-      </div> */}
+      </div>
       <div className="course-title text-center">
         <h3>Thêm khóa học</h3>
       </div>
-      <div className="mt-5 form-group">
-        <input className='form-control mb-2' type="search" placeholder="Nhập vào tại khoản hoặc tên người dùng" />
-        <button className="btn btn-dark btn-sm">Tìm kiếm</button>
-      </div>
+      <form className="mt-5">
+        <div className="form-group">
+          <input
+            className="form-control mb-2"
+            type="search"
+            placeholder="Nhập vào tại khoản hoặc tên người dùng"
+          />
+          <button className="btn btn-dark btn-sm">Tìm kiếm</button>
+        </div>
+      </form>
       <div className="d-flex mt-5">
         <div className="table-responsive border border-dark">
-          <table className="table align-middle">
+          <table className="table table-striped align-middle">
             <thead>
               <tr>
                 <th>STT</th>
@@ -89,9 +96,15 @@ export default function CourseManagement({}: Props) {
                 <td>100k</td>
                 <td>bruh danh tính</td>
                 <td>
-                  <button className='btn btn-success m-2'>Ghi danh</button>
-                  <button className='btn btn-warning m-2'>Cập nhật</button>
-                  <button className='btn btn-danger m-2'>X</button>
+                  <button
+                    data-bs-toggle="modal"
+                    data-bs-target="#modalId"
+                    className="btn btn-success m-2"
+                  >
+                    Ghi danh
+                  </button>
+                  <button className="btn btn-warning m-2">Cập nhật</button>
+                  <button className="btn btn-danger m-2">X</button>
                 </td>
               </tr>
             </tbody>
