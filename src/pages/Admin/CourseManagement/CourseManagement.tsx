6a8b36344ee2a6e729b154c3039f8242876e249a
khaitran <akashikazuo99@gmail.com>
2022-10-27 23:10:24 +0700

    update course again

--- src/pages/Admin/CourseManagement/CourseManagement.tsx
+++ src/pages/Admin/CourseManagement/CourseManagement.tsx
@@ -5,7 +5,7 @@ type Props = {}
 export default function CourseManagement({}: Props) {
   return (
     <div className="container course-management">
-      <div className="userContainer mt-5 d-none">
+      {/* <div className="userContainer mt-5 d-none">
         <button
           type="button"
           className="btn btn-dark btn-lg"
@@ -56,12 +56,12 @@ export default function CourseManagement({}: Props) {
             </div>
           </div>
         </div>
-      </div>
-      <div className="course-title">
+      </div> */}
+      <div className="course-title text-center">
         <h3>Thêm khóa học</h3>
       </div>
-      <div className="mt-5">
-        <input type="search" placeholder="Họ tên ghi danh" />
+      <div className="mt-5 form-group">
+        <input className='form-control mb-2' type="search" placeholder="Nhập vào tại khoản hoặc tên người dùng" />
         <button className="btn btn-dark btn-sm">Tìm kiếm</button>
       </div>
       <div className="d-flex mt-5">
@@ -70,23 +70,29 @@ export default function CourseManagement({}: Props) {
             <thead>
               <tr>
                 <th>STT</th>
-                <th>Tai Khoan</th>
-                <th>Mat Khau</th>
-                <th>Ho Ten</th>
-                <th>Email</th>
-                <th>So Dien Thoai</th>
-                <th>Thao Tac</th>
+                <th>Mã khóa học</th>
+                <th>Tên khóa học</th>
+                <th>Hình ảnh</th>
+                <th>Lượt xem</th>
+                <th>Người tạo</th>
+                <th>Thao Tác</th>
               </tr>
             </thead>
             <tbody>
               <tr>
-                <td></td>
-                <td></td>
-                <td></td>
-                <td></td>
-                <td></td>
-                <td></td>
-                <td></td>
+                <td>1</td>
+                <td>ITec0123</td>
+                <td>khóa bruh</td>
+                <td>
+                  <img src="https://picsum.photos/50/50" alt="..." />
+                </td>
+                <td>100k</td>
+                <td>bruh danh tính</td>
+                <td>
+                  <button className='btn btn-success m-2'>Ghi danh</button>
+                  <button className='btn btn-warning m-2'>Cập nhật</button>
+                  <button className='btn btn-danger m-2'>X</button>
+                </td>
               </tr>
             </tbody>
           </table>
